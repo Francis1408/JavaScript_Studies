be1f47488778ba8be564798777a452fcb5be495e
Francisco <franciscoabreu1408@gmail.com>
2023-04-11 13:56:28 -0300

    JS1 done

--- JS1-Project/resolve-equacao.js
+++ JS1-Project/resolve-equacao.js
@@ -9,22 +9,64 @@ function calculaRaizes(delta, valor_a, valor_b){
 
 }
 
+function calculaVertice(valor_a, valor_b, delta) {
 
+    let vertice_x = document.querySelector('#vertice-x');
+    let vertice_y = document.querySelector('#vertice-y');
 
-function calculaDiscriminante(){
+    vertice_x.value = -(valor_b/(2*valor_a));
+    vertice_y.value = -(delta/(4*valor_a));
 
-    let valor_a = document.querySelector('#coeficiente-a').value;
-    let valor_b = document.querySelector('#coeficiente-b').value;
-    let valor_c = document.querySelector('#coeficiente-c').value;
-    let delta = document.querySelector('#resultado-delta');
+}
+
+function calculaDiscriminante(valor_a, valor_b, valor_c, delta) {
 
     delta.value = valor_b*valor_b - 4*valor_a*valor_c;
 
     calculaRaizes(delta.value, valor_a, valor_b);
+    calculaVertice(valor_a, valor_b, delta.value);
+    defineQuantidadeRaizes(delta.value);
+}
+
+function definePosicaoParabola(valor_a) {
+
+    let pos_parabola = document.querySelector('#parabola-pos');
+
+    if(valor_a < 0) pos_parabola.value = 'Para baixo';
+    else pos_parabola.value = 'Para cima'; 
+     
+}
+
+function defineQuantidadeRaizes(delta) {
+
+    let num_raizes = document.querySelector('#num-raizes');
+
+    if(delta < 0) num_raizes.value = 0;
+    else if(delta == 0) num_raizes.value = 1;
+    else num_raizes.value = 2;
+
+}
+
+
+function verificaA() {
+
+    let valor_a = document.querySelector('#coeficiente-a').value;
+
+    if(valor_a != 0) {
+
+        let valor_b = document.querySelector('#coeficiente-b').value;
+        let valor_c = document.querySelector('#coeficiente-c').value;
+        let delta = document.querySelector('#resultado-delta');
+        calculaDiscriminante(valor_a, valor_b, valor_c, delta);
+        definePosicaoParabola(valor_a);
+    }
+
+    else window.alert("Função inserida não é de segundo grau");
+    
 }
 
 
 
 let botaoResultadoEl =  document.querySelector('#resolver');
 
-botaoResultadoEl.addEventListener('click', calculaDiscriminante);
+botaoResultadoEl.addEventListener('click', verificaA);
