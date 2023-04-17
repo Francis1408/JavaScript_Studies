09df0f1626f002a71da05b8f3cc3c8e94ffd267c
Francisco <franciscoabreu1408@gmail.com>
2023-04-17 17:44:06 -0300

    Solving challange 3 JS3

--- JS3-Project/balaozinho.js
+++ JS3-Project/balaozinho.js
@@ -1,7 +1,14 @@
 
+let marcacao_selecionada = 0;
+
 let marcacoesEl = document.querySelectorAll('.marcacao');
 let balaozinhoEl = document.querySelector('#balaozinho');
 let botaoDefinirEl = document.querySelector('#definir-marcacao');
+
+let marcacao_xEl = document.querySelector('#marcacao-x');
+let marcacao_yEl = document.querySelector('#marcacao-y');
+let marcacao_larguraEl = document.querySelector('#marcacao-largura');
+let marcacao_alturaEl = document.querySelector('#marcacao-altura');
 
 // Evento de marcação 
 
@@ -11,7 +18,7 @@ for (let marcacaoEl of marcacoesEl) {
 
         let triggeredEl = e.currentTarget;
 
-        console.log(triggeredEl);
+        //console.log(triggeredEl);
         let title = triggeredEl.dataset.titulo;
         let body = triggeredEl.dataset.conteudo;
 
@@ -32,18 +39,53 @@ for (let marcacaoEl of marcacoesEl) {
         balaozinhoEl.style.left = `${e.pageX}px`;
         balaozinhoEl.style.top = `${e.pageY}px`;
     });
+
+    marcacaoEl.addEventListener('click', (e) => {
+
+        let marcacoesArray = Array.from(marcacoesEl);
+        marcacao_selecionada = marcacoesArray.indexOf(e.currentTarget);
+
+        for(let i = 0; i < marcacoesEl.length; i++) {
+
+            marcacoesEl[i].classList.remove('selecionada');
+        }
+        e.currentTarget.classList.add('selecionada');
+       //console.log(marcacao_selecionada);
+    })
 }
 
 // Evento de definir marcação
 
+/*
+
 botaoDefinirEl.addEventListener('click', () => {
 
-    let marcacao_xEl = document.querySelector('#marcacao-x').value;
-    let marcacao_yEl = document.querySelector('#marcacao-y').value;
-    let marcacao_larguraEl = document.querySelector('#marcacao-largura').value;
-    let marcacao_alturaEl = document.querySelector('#marcacao-altura').value;
-
-    marcacoesEl[0].style = `width: ${marcacao_larguraEl}px; height: ${marcacao_alturaEl}px; top: ${marcacao_yEl}px; left: ${marcacao_xEl}px`;
-
+    marcacoesEl[0].style = `width: ${marcacao_larguraEl.value}px; height: ${marcacao_alturaEl.value}px; top: ${marcacao_yEl.value}px; left: ${marcacao_xEl.value}px`;
 
 });
+*/ 
+
+marcacao_xEl.addEventListener('change', (e) => {
+
+    marcacoesEl[marcacao_selecionada].style.top = `${e.currentTarget.value}px`
+
+});
+
+marcacao_yEl.addEventListener('change', (e) => {
+
+    marcacoesEl[marcacao_selecionada].style.left = `${e.currentTarget.value}px`
+
+});
+
+marcacao_alturaEl.addEventListener('change', (e) => {
+
+    marcacoesEl[marcacao_selecionada].style.height = `${e.currentTarget.value}px`
+
+});
+
+marcacao_larguraEl.addEventListener('change', (e) => {
+
+    marcacoesEl[marcacao_selecionada].style.width = `${e.currentTarget.value}px`
+
+});
+
