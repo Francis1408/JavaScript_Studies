b725892a5714991c67aadc4606d1d34b8215353e
Francisco <franciscoabreu1408@gmail.com>
2023-04-19 17:28:28 -0300

    HMTL1 done, solving HTML2

---
+++ HTML2-Project/planilhaPirata.js
@@ -0,0 +1,17 @@
+// Moedas Elements
+let moedas_valorEl = document.querySelector("#moedas-valor");
+let moedas_qtdEl = document.querySelector("#moedas-qtd");
+let moedas_totalEl = document.querySelector("#moedas-total");
+
+// Coroas Elements
+let coroas_valorEl = document.querySelector("#coroas-valor");
+let coroas_qtdEl = document.querySelector("#coroas-qtd");
+let coroas_totalEl = document.querySelector("#coroas-total");
+
+// Calices Elements
+let calices_valorEl = document.querySelector("#calices-valor");
+let calices_qtdEl = document.querySelector("#calices-qtd");
+let calices_total = document.querySelector("#calices-total");
+
+
+
