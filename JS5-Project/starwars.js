8f55f9bb5d7b09a4b546762e64dc1f2342955761
Francisco <franciscoabreu1408@gmail.com>
2023-05-18 22:02:57 -0300

    Doing JS5

---
+++ JS5-Project/starwars.js
@@ -0,0 +1,10 @@
+// Seu javascript aqui :)
+// Use a Star Wars API: https://swapi.dev/
+// para fazer uma requisição assíncrona e:
+//  - Pegar a lista de filmes (AJAX) e preencher no HTML
+//  - Quando um filme for clicado, exibir sua introdução
+import { play } from "./music.js"
+
+const API_ENDPOINT = 'https://swapi.dev/api'
+
+play({audioUrl: 'audio/tema-sw.mp3', coverImageUrl: 'imgs/logo.svg', title: 'Intro', artist: 'John Williams'}, document.body);
